791535d11ba9b6b4d8a3c138a6cdda280d2d313f
XinYun2020 <xinyunzhang2016@gmail.com>
2022-11-16 00:34:40 +1100

    install dep add src folders

---
+++ frontend_react/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom"; // help us connect to HTML file
+
+import App from "./App";
+import "../styles/global.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
